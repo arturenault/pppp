bc08ae91eee4f0c054e6e966a7f276e18ee08909
Artur Upton Renault <artur.renault@gmail.com>
2015-09-19 14:21:43 -0400

    Very good door policy; ++comments

--- src/pppp/g9/Player.java
+++ src/pppp/g9/Player.java
@@ -26,6 +26,7 @@ public class Player implements pppp.sim.Player {
     private int side = 0;
     private int piper_at_door = -1;
     private int[] pos_index = null;
+    private Point door_pos = null;
     private Point[][] pos = null;
     private Random gen = new Random();
     private Point[] piper_rats;
@@ -64,7 +65,7 @@ public class Player implements pppp.sim.Player {
         pos_index = new int [n_pipers];
         piper_rats = new Point[n_pipers];
         piper_positions = new Point[n_pipers];
-        
+
         for (int p = 0 ; p != n_pipers ; ++p) {
             // spread out at the door level
             double door = 0.0;
@@ -73,10 +74,10 @@ public class Player implements pppp.sim.Player {
             boolean neg_y = id == 2 || id == 3;
             boolean swap  = id == 1 || id == 3;
             // first and third position is at the door
-            pos[p][0] = pos[p][2] = point(door, side * 0.5, neg_y, swap);
+            door_pos = pos[p][0] = pos[p][2] = point(door, side * 0.5, neg_y, swap);
             // second position is chosen randomly in the rat moving area
             pos[p][1] = null;
-            
+
             pos[p][3] = point(door, side * 0.5 + 10, neg_y, swap);
             // start with first position
             pos_index[p] = 0;
@@ -104,29 +105,40 @@ public class Player implements pppp.sim.Player {
                     // if position is reached
                     if ( Math.abs(src.x - dst.x) < 0.000001 &&
                             Math.abs(src.y - dst.y) < 0.000001 ||
-                            pos_index[p] == 1 && within(src, piper_rats[p])) {
+
+                            // Or the piper has caught his rat
+                            pos_index[p] == 1 && within(src, piper_rats[p]) ||
+
+                            // Or this guy is at the door and the rats at the door have been captured
+                            piper_at_door == p && !withRats(door_pos, rats)) { 
 
                         // get next position
                         if (++pos_index[p] == pos[p].length) {
                             pos_index[p] = 0;
-                            piper_rats[p] = null;
                             piper_at_door = -1;
                         }
 
                         if (pos_index[p] == 3) {
-                            if (piper_at_door > 0 && piper_at_door != p) {
+                            if (piper_at_door >= 0 && piper_at_door != p) {
                                 pos_index[p] = 1;
                             } else {
                                 piper_at_door = p;
                             }
                         }
 
+                        if (pos_index[p] == 1) {
+                            piper_rats[p] = null;
+                        }
+
                         dst = pos[p][pos_index[p]];
                         // generate a new position if random
                         if (dst == null) {
                             dst = getNearestRat(src, rats, p);
+                            if (within(dst, door_pos)) {
+                               pos_index[p] = 3; 
+                            }
                         }
-                    }
+                            }
                     // get move towards position
                     moves[p] = move(src, dst, pos_index[p] > 1);
                 }
@@ -138,24 +150,43 @@ public class Player implements pppp.sim.Player {
 
     public Point getNearestRat(Point piper_pos, Point[] rats, int piper_index){
         double min = Double.MAX_VALUE;
-        int min_index = 0;
+        int min_index = -1;
         for(int i = 0 ; i < rats.length; i++){
-            double dis = calDistance(piper_pos, rats[i]);
-            if(dis < min && (piper_rats[piper_index] != null || dis > 10)){
-                boolean already_assigned = false;
-                for (int j = 0; j < piper_rats.length; j++) {
-                    if (piper_rats[j] != null && sameRat(rats[i], piper_rats[j])) {
-                        if (j == piper_index) {
-                            piper_rats[j] = rats[i];
-                            return rats[i];
-                        } else already_assigned = true;
+
+            // ignore rats near door if there is already a piper there 
+            if (piper_at_door < 0 || !within(door_pos, rats[i])) {
+                double dis = calDistance(piper_pos, rats[i]);
+
+                // if piper_rats[piper_index] is null, then the piper just got out of the door
+                if(dis < min && (piper_rats[piper_index] != null || dis > 10)){
+                    boolean already_assigned = false;
+                    
+                    // check that another player isn't already handling this rat
+                    for (int j = 0; j < piper_rats.length; j++) {
+                        if (piper_rats[j] != null && sameRat(rats[i], piper_rats[j])) {
+                            if (j == piper_index) {
+                                // This piper owns this rat, update position of rat
+                                piper_rats[j] = rats[i];
+                                return rats[i];
+
+                            // Otherwise someone else does
+                            } else {
+                                already_assigned = true;
+                                break;
+                            }
+                        }
                     }
-                }
-                if (!already_assigned) {
-                    min = dis;
-                    min_index = i;
-                }
-            } 
+
+                    // if nobody else is handling this rat, this piper can take it.
+                    if (!already_assigned) {
+                        min = dis;
+                        min_index = i;
+                    }
+                } 
+            }
+        }
+        if (min_index == -1) {
+            min_index = gen.nextInt(rats.length);
         }
         piper_rats[piper_index] = rats[min_index];
         return rats[min_index];
@@ -191,6 +222,6 @@ public class Player implements pppp.sim.Player {
     }
 
     public boolean sameRat(Point p1, Point p2) {
-        return calDistance(p1, p2) <= 0.1;
+        return calDistance(p1, p2) <= 1;
     }
 }
